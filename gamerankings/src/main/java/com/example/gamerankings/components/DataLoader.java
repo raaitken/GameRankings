6eb3e3c5a29a02701d9d32e0fd4b6e70ee7ec2c0
ross-aitken <raaitken@gmail.com>
2023-06-22 15:16:27 +0100

    GameUser table now showing in API

--- gamerankings/src/main/java/com/example/gamerankings/components/DataLoader.java
+++ gamerankings/src/main/java/com/example/gamerankings/components/DataLoader.java
@@ -1,9 +1,11 @@
 package com.example.gamerankings.components;
 
 import com.example.gamerankings.models.Game;
+import com.example.gamerankings.models.GameUser;
 import com.example.gamerankings.models.Platform;
 import com.example.gamerankings.models.User;
 import com.example.gamerankings.repositories.GameRepository;
+import com.example.gamerankings.repositories.GameUserRepository;
 import com.example.gamerankings.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
@@ -21,6 +23,8 @@ public class DataLoader implements ApplicationRunner {
     GameRepository gameRepository;
     @Autowired
     UserRepository userRepository;
+    @Autowired
+    GameUserRepository gameUserRepository;
 
     ArrayList<Platform> platforms = new ArrayList<>();
 
@@ -34,7 +38,18 @@ public class DataLoader implements ApplicationRunner {
         Game ff16 = new Game("Final Fantasy XVI", "imgsrc", "RPG", "Description", 2023, platforms);
         gameRepository.save(ff16);
 
+        Game diablo4 = new Game("Diablo 4", "imgsrc", "ARPG", "Description", 2023, platforms);
+        gameRepository.save(diablo4);
+
         User ross = new User("rossaitken", "password");
         userRepository.save(ross);
+
+        User sam = new User("samhouston", "password");
+        userRepository.save(sam);
+
+        GameUser gameUser = new GameUser(ff16, ross);
+        GameUser gu1 = new GameUser(diablo4, sam);
+        gameUserRepository.save(gameUser);
+        gameUserRepository.save(gu1);
     }
 }
--- gamerankings/src/main/java/com/example/gamerankings/models/GameUser.java
+++ gamerankings/src/main/java/com/example/gamerankings/models/GameUser.java
@@ -1,5 +1,7 @@
 package com.example.gamerankings.models;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 import javax.persistence.*;
 
 @Entity
@@ -10,22 +12,56 @@ public class GameUser {
     private GameUserId id = new GameUserId();
 
     @ManyToOne
+    @JsonBackReference
     @MapsId("game_id")
     private Game game;
 
     @ManyToOne
+    @JsonBackReference
     @MapsId("user_id")
     private User user;
 
     private int rating;
 
-    public GameUser(Game game, User user, int rating) {
+    public GameUser(Game game, User user) {
         this.game = game;
         this.user = user;
-        this.rating = rating;
+        this.rating = 1;
     }
 
     public GameUser(){
 
+    }
+
+    public GameUserId getId() {
+        return id;
+    }
+
+    public void setId(GameUserId id) {
+        this.id = id;
+    }
+
+    public Game getGame() {
+        return game;
+    }
+
+    public void setGame(Game game) {
+        this.game = game;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
     }
 }
