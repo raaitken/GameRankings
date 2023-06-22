9d6ae9309098d1bf2fd75860dfb8ab53dfa1c420
ross-aitken <raaitken@gmail.com>
2023-06-22 16:15:58 +0100

    Elo calculation working

--- gamerankings/src/main/java/com/example/gamerankings/models/GameUser.java
+++ gamerankings/src/main/java/com/example/gamerankings/models/GameUser.java
@@ -64,4 +64,32 @@ public class GameUser {
     public void setRating(int rating) {
         this.rating = rating;
     }
+
+    public static float calculateProbability(float rating1, float rating2){
+        return 1.0f * 1.0f / (1 + 1.0f * (float) (Math.pow(10, 1.0f * (rating1 - rating2) / 400)));
+    }
+
+    public static void EloRating(float Ra, float Rb, int K, boolean d){
+        float Pb = calculateProbability(Ra, Rb);
+
+        float Pa = calculateProbability(Rb, Ra);
+
+        if (d) {
+            Ra = Ra + K * (1 - Pa);
+            Rb = Rb + K * (0 - Pb);
+        }
+
+        else {
+            Ra = Ra + K * (0 - Pa);
+            Rb = Rb + K * (1 - Pb);
+        }
+
+        System.out.print("Updated Ratings:-\n");
+
+        System.out.print(
+                "Ra = "
+                        + (Math.round(Ra * 1000000.0) / 1000000.0)
+                        + " Rb = "
+                        + Math.round(Rb * 1000000.0) / 1000000.0);
+    }
 }
--- gamerankings/src/test/java/com/example/gamerankings/GamerankingsApplicationTests.java
+++ gamerankings/src/test/java/com/example/gamerankings/GamerankingsApplicationTests.java
@@ -1,6 +1,7 @@
 package com.example.gamerankings;
 
 import com.example.gamerankings.models.Game;
+import com.example.gamerankings.models.GameUser;
 import com.example.gamerankings.models.Platform;
 import com.example.gamerankings.models.User;
 import com.example.gamerankings.repositories.GameRepository;
@@ -11,6 +12,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class GamerankingsApplicationTests {
@@ -37,6 +40,17 @@ class GamerankingsApplicationTests {
 
 		User user1 = new User("samhouston", "password");
 		userRepository.save(user1);
+	}
+
+	@Test
+	public void canCalculateElo(){
+		float Ra = 1200, Rb = 1000;
+
+		int K = 30;
+		boolean d = true;
+
+		GameUser.EloRating(Ra, Rb, K, d);
+		assertEquals(1207.20, Ra, 0);
 	}
 
 }
