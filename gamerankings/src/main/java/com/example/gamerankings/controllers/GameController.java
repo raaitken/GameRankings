d54cb8642f20272be1fe20ae60cdbd1116bf7116
ross-aitken <raaitken@gmail.com>
2023-06-22 15:29:49 +0100

    Added get for single users and games

--- gamerankings/src/main/java/com/example/gamerankings/controllers/GameController.java
+++ gamerankings/src/main/java/com/example/gamerankings/controllers/GameController.java
@@ -6,9 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class GameController {
@@ -19,5 +21,10 @@ public class GameController {
     @GetMapping(value = "/games")
     public ResponseEntity<List<Game>> getAllGames(){
         return new ResponseEntity<>(gameRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/games/{id}")
+    public ResponseEntity<Optional<Game>> getPirate(@PathVariable Long id){
+        return new ResponseEntity<>(gameRepository.findById(id), HttpStatus.OK);
     }
 }
