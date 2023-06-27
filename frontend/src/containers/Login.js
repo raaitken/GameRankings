c860a5dc35a9c480e21716796091cf76641e726c
codeclanstudent <codeclanstudent@codeclanstudents-MacBook-Pro.local>
2023-06-27 13:50:43 +0100

    Jazzy login page

--- frontend/src/containers/Login.js
+++ frontend/src/containers/Login.js
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import './Login.css';
 import Button from 'react-bootstrap/Button';
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -7,25 +6,17 @@ import profileImage from './images/jb.jpeg';
 
 function Login() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <div className="login-form">
-          <div className="profile-picture">
-            <img src={profileImage} alt="Profile" />
-          </div>
-          <div className="user-info">
-            <span className="user-name">Name</span>
-          </div>
-          <div className="form-group">
-            <input type="text" placeholder="Username" />
-            <input type="password" placeholder="Password" />
-            <Button className="login-button">Login</Button>
-            <div className="forgot-password">
-              <Button variant="link">Forgotten Password?</Button>
-            </div>
-          </div>
+    <div className="blurred-box">
+      <div className="user-login-box">
+        <span className="user-icon"></span>
+        <div className="user-name">Name</div>
+        <input className="user-password" type="text" placeholder="Username" />
+        <input className="user-password" type="password" placeholder="Password" />
+        <Button className="login-button">Login</Button>
+        <div className="forgot-password">
+          <Button variant="link">Forgotten Password?</Button>
         </div>
-      </header>
+      </div>
     </div>
   );
 }
