a62dab32e38c343be2fe7ab3243cd26cc473eaaa
EuanCrawford <euan.crawford@hotmail.co.uk>
2023-06-27 11:31:07 +0100

    added navbar links

--- frontend/server/server.js
+++ frontend/server/server.js
@@ -2,18 +2,39 @@ const express = require('express');
 const app = express();
 const MongoClient = require('mongodb').MongoClient;
 const createRouter = require('./helpers/create_router');
-const cors = require('cors');
+// const cors = require('cors');
 
-app.use(cors())
+// app.use(cors())
 app.use(express.json())
+
+var db = null;
 
 MongoClient.connect('mongodb://127.0.0.1:27017', { useUnifiedTopology: true })
     .then((client) => {
-        const db = client.db('gamerankings');
+        db = client.db('gamerankings');
         const gamesCollection = db.collection('games');
-        const gamesRouter = createRouter(gamesCollection);
-        app.use('/api/games', gamesRouter);
     }).catch(console.error)
+
+const addToMongo = (gamesArray) => {
+    for (results in gamesArray) {
+        for (game in results) {
+            db.collection('games').insertOne(game);
+        }
+    }
+}
+
+const getAllGames = () => {
+    const allGames = [];
+    for (let i = 1; i <= 100; i++) {
+        const newFetch = fetch("https://api.rawg.io/api/games?page=" + i + "&page_size=40&key=362bb5d0da074b7ab10c431a707dd76e&")
+        .then((response) => response.json())
+        allGames.push(newFetch);
+    }
+    Promise.all(allGames)
+    .then((data) => addToMongo(allGames));
+}
+
+getAllGames();
 
 app.listen(9000, function(){
     console.log(`app listening on port ${this.address().port}`);
