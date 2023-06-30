3e2256c20211bd9c878df95298f8a34c983d2da6
ross-aitken <raaitken@gmail.com>
2023-06-30 15:31:21 +0100

    Trying to find a game via the slug

--- gamerankings/src/main/java/com/example/gamerankings/controllers/GameUserController.java
+++ gamerankings/src/main/java/com/example/gamerankings/controllers/GameUserController.java
@@ -1,6 +1,8 @@
 package com.example.gamerankings.controllers;
 
+import com.example.gamerankings.models.Game;
 import com.example.gamerankings.models.GameUser;
+import com.example.gamerankings.models.User;
 import com.example.gamerankings.repositories.GameRepository;
 import com.example.gamerankings.repositories.GameUserRepository;
 import com.example.gamerankings.repositories.UserRepository;
@@ -10,6 +12,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
+
+import static java.lang.Double.parseDouble;
 
 @RestController
 public class GameUserController {
@@ -26,21 +31,23 @@ public class GameUserController {
         return new ResponseEntity<>(gameUserRepository.findAll(), HttpStatus.OK);
     }
 
-//    @PostMapping(value = "/gameratings")
-//    public ResponseEntity<GameUser> postGameUser(@RequestBody Map<String, Object> payload){
-//        Object game = payload.get("game");
-//        Object user = payload.get("user");
-//        Object rating = payload.get("rating");
-//        GameUser newGameUser = new GameUser((Game) game, (User) user, parseDouble(rating.toString()));
-//        gameUserRepository.save(newGameUser);
-//        return new ResponseEntity<>(newGameUser, HttpStatus.CREATED);
-//    }
-
     @PostMapping(value = "/gameratings")
-    public ResponseEntity<GameUser> postGameUser(@RequestBody GameUser gameUser){
-        gameUserRepository.save(gameUser);
-        return new ResponseEntity<>(gameUser, HttpStatus.CREATED);
+    public ResponseEntity<GameUser> postGameUser(@RequestBody Map<String, Object> payload){
+        Object gameSlug = payload.get("game");
+        Object user = payload.get("user");
+        Object rating = payload.get("rating");
+        System.out.println(gameSlug);
+        Game game = gameRepository.findBySlug(gameSlug.toString());
+        GameUser newGameUser = new GameUser((Game) game, (User) user, parseDouble(rating.toString()));
+        gameUserRepository.save(newGameUser);
+        return new ResponseEntity<>(newGameUser, HttpStatus.CREATED);
     }
+
+//    @PostMapping(value = "/gameratings")
+//    public ResponseEntity<GameUser> postGameUser(@RequestBody GameUser gameUser){
+//        gameUserRepository.save(gameUser);
+//        return new ResponseEntity<>(gameUser, HttpStatus.CREATED);
+//    }
 
     @PatchMapping(value = "/gameratings/{id}")
     public ResponseEntity<GameUser> updateGameUser(@RequestBody GameUser gameUser) {
